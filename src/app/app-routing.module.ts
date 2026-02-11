c3cbdb8b52792f574b454b1fc02fbf3b0e091290
nour <bouslimi.nourelhouda@esprit.tn>
2026-02-11 20:30:53 +0100

    template freelancers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component:HomeComponent},
+  // les path te3 les modules
+  { path: 'users', loadChildren: () => import('./modules/users/users.module').then(u => u.UsersModule) },
+  { path: 'profiles', loadChildren: () => import('./modules/profile/profile.module').then(p => p.ProfileModule) },
+  { path: 'missions', loadChildren: () => import('./modules/mission/mission.module').then(m => m.MissionModule) },
+  { path: 'contrats', loadChildren: () => import('./modules/contrat/contrat.module').then(c=> c.ContratModule) },
+  { path: 'reviews', loadChildren: () => import('./modules/review/review.module').then(r=> r.ReviewModule) },
+  { path: 'chats', loadChildren: () => import('./modules/chat/chat.module').then(ch =>ch.ChatModule) },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
